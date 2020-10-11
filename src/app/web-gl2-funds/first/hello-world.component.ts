d7ef273a109825c2b229d288aa23950a2fe3f3c9
9motom6 <9motom6@gmail.com>
2020-10-11 22:42:22 +0200

    Draw 50 random rects

--- src/app/web-gl2-funds/first/hello-world.component.ts
+++ src/app/web-gl2-funds/first/hello-world.component.ts
@@ -30,19 +30,8 @@ export class HelloWorldComponent implements AfterViewInit {
         const resolutionUniformLocation: WebGLUniformLocation = gl.getUniformLocation(program, "u_resolution");
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-        // three 2d points
-        const positions: number[] = [
-            10, 20,
-            80, 20,
-            10, 30,
-            10, 30,
-            80, 20,
-            80, 30,
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+        const colorLocation = gl.getUniformLocation(program, "u_color");
 
-        const vao: WebGLVertexArrayObject = gl.createVertexArray();
-        gl.bindVertexArray(vao);
         gl.enableVertexAttribArray(positionAttributeLocation);
 
         const size = 2;          // 2 components per iteration
@@ -63,13 +52,48 @@ export class HelloWorldComponent implements AfterViewInit {
 
         // Pass in the canvas resolution so we can convert from
         // pixels to clip space in the shader
-
         gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-        // Bind the attribute/buffer set we want.
-        gl.bindVertexArray(vao);
 
-        const primitiveType: number = gl.TRIANGLES;
-        const count: number = 6;
+        // draw 50 random rectangles in random colors
+        for (let ii = 0; ii < 50; ++ii) {
+            this.drawRandomRect(gl, colorLocation, offset);
+        }
+    }
+
+    private drawRandomRect(gl: WebGL2RenderingContext, colorLocation: WebGLUniformLocation, offset: number): void {
+        // Put a rectangle in the position buffer
+        this.setRectangle(
+            gl, this.randomInt(300), this.randomInt(300), this.randomInt(300), this.randomInt(300));
+
+        // Set a random color.
+        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+
+        // Draw the rectangle.
+        const primitiveType = gl.TRIANGLES;
+        // const offset = 0;
+        const count = 6;
         gl.drawArrays(primitiveType, offset, count);
     }
+
+// Returns a random integer from 0 to range - 1.
+    private randomInt(range: number): number {
+        return Math.floor(Math.random() * range);
+    }
+
+    // Fill the buffer with the values that define a rectangle.
+    private setRectangle(gl, x, y, width, height): void {
+        let x1 = x;
+        let x2 = x + width;
+        let y1 = y;
+        let y2 = y + height;
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+            x1, y1,
+            x2, y1,
+            x1, y2,
+            x1, y2,
+            x2, y1,
+            x2, y2,
+        ]), gl.STATIC_DRAW);
+    }
 }
+
